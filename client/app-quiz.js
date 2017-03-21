49db54f2442289e221e296e340365144eae91091
Julius Alvarado <angularjs.uiux@gmail.com>
2017-03-20 17:55:31 -0700

    finished score calculation and raw html share links

--- client/app-quiz.js
+++ client/app-quiz.js
@@ -16,7 +16,7 @@
         $scope.score = 0;
         $scope.activeQuestion = -1;
         $scope.activeQuestionAnswered = 0;
-        $scope.percentage = 0;
+        $scope.percentScore = 0;
 
         $scope.selectAnswer = function (indexQuestion, indexAnswer) {
             var questionState = $scope.myQuestions[indexQuestion].questionState;
@@ -35,18 +35,32 @@
                 // now that user has clicked on an answer I now set .questionState
                 $scope.myQuestions[indexQuestion].questionState = 'answered';
             }
+
+            $scope.percentScore = (100 * ($scope.score / $scope.totalQuestions))
+                .toFixed(2);
         };
 
         $scope.isSelected = function (qIndex, aIndex) {
             return $scope.myQuestions[qIndex].selectedAnswer === aIndex;
         };
 
-        $scope.isCorrect = function(qIndex, aIndex){
+        $scope.isCorrect = function (qIndex, aIndex) {
             return $scope.myQuestions[qIndex].correctAnswer === aIndex;
         };
 
-        $scope.selectContinue = function(){
+        $scope.selectContinue = function () {
             return $scope.activeQuestion += 1;
+        };
+
+        $scope.createShareLinks = function (percent) {
+            var url = 'http://php1.julius3d.com';
+            var emailLink = '<a href="mailto:?subject=Quiz Score.&amp;body=Beat my '+percent+
+                '% quiz score at '+url+' studios." class="btn btn-sm btn-warning email">Email</a>';
+            var tweetLink = '<a href="http://twitter.com/share?text=I scored '+percent+' on my AngularJS quiz. ' +
+                'Beat my score at &hashtags=ngQuiz&url='+url+'" target="_blank" class="btn btn-sm btn-info twitter">Tweet</a>';
+
+            var newMarkup = emailLink + tweetLink;
+            return $sce.trustAsHtml(newMarkup);
         };
 
         activate();
